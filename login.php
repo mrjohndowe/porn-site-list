1472e80d4914b62ede7651ef31859d4974c18a31
mrjohndowe <59076389+mrjohndowe@users.noreply.github.com>
2021-12-21 16:40:15 -0700

    Update login.php

--- login.php
+++ login.php
@@ -17,9 +17,7 @@
 		$href = $protocol.$sub.$host.'.'.$tld.'/';
 		$path = $pathLink == TRUE && $row['path'] != "null" ? $row['path'] : '';
 		$header = $href.$path;
-		
-<<<<<<< HEAD
-<<<<<<< HEAD
+        
 		 $approvedDevices = [
             '192.168.1.70', 
 			'192.168.1.25', 
@@ -31,12 +29,7 @@
 			header("Location: $header");
 			exit;
 		}
-		
-		
-=======
->>>>>>> master
-=======
->>>>>>> master
+
 		$clickCount = $row['click_count'];
 		$q = "UPDATE sites SET click_count = :click WHERE id = :id";
 		$p = [':id' => $id, ':click' => $clickCount + 1];
